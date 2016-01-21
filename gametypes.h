579b16b8a309b57bb9d0418af83e34e001a9a874
Travis <tssund93@gmail.com>
2016-01-21 13:37:37 -0500

    lined up macro definitions

--- gametypes.h
+++ gametypes.h
@@ -1,17 +1,17 @@
 #ifndef __GAMETYPES_H
 #define __GAMETYPES_H
 
-#define WIN_LENGTH	4
-#define WIN_SCORE	100000
+#define WIN_LENGTH		4
+#define WIN_SCORE		100000
 #define ALPHABETA_LEVEL	7
 #define STRATEGY_BONUS	500
 #define TIMEBANK_LOW	2000
 
-#define TRUE 1
-#define FALSE 0
+#define TRUE	1
+#define FALSE	0
 
-#define MAX_FIELD_COLUMNS 10
-#define MAX_FIELD_ROWS 10
+#define MAX_FIELD_COLUMNS	10
+#define MAX_FIELD_ROWS		10
 
 struct Settings {
 	int		timebank;
