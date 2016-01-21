370ea997eb7692561d1bccd3936656ae446bf268
Travis <tssund93@gmail.com>
2016-01-21 17:17:07 -0500

    made a separate header file for any string functions i write

--- fourparser.c
+++ fourparser.c
@@ -5,25 +5,6 @@
 #include "fourparser.h"
 #include "fourgame.h"
 
-char *trim(char *str)
-{
-	char *end;
-
-	while (isspace(*str))
-		str++;
-
-	if (*str == 0)
-		return str;
-
-	end = str + strlen(str) - 1;
-	while (end > str && isspace(*end))
-		end--;
-
-	*(end + 1) = 0;
-
-	return str;
-}
-
 void dump_game()
 {
 	fprintf(stderr, "timebank: %d\n", game.settings.timebank);
---
+++ mystrlib.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char *trim(char *str)
+{
+	char *end;
+
+	while (isspace(*str))
+		str++;
+
+	if (*str == 0)
+		return str;
+
+	end = str + strlen(str) - 1;
+	while (end > str && isspace(*end))
+		end--;
+
+	*(end + 1) = 0;
+
+	return str;
+}
